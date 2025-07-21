d67e42fac22f7878de2db5e1438e1d3b3ea67c79
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2025-07-20 21:50:57 -0400

    [CHG] improve controls in DTO with transform and validator [CHG] update version of core Nest to V11

--- src/customers/dto/customer.dto.ts
+++ src/customers/dto/customer.dto.ts
@@ -1,9 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateCustomerDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   lastname: string;
+
+  @IsString()
+  @IsNotEmpty()
   phone: string;
 }
 
